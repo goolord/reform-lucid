44deb539453a0cccff6c6b5842123f42fc22fb9e
Jeremy Shaw <jeremy@n-heptane.com>
2015-12-18 15:09:15 -0600

    0.2.4.1: split into separate repo. Added README.md and .travis.yml

--- default.nix
+++ default.nix
@@ -3,9 +3,11 @@
 }:
 mkDerivation {
   pname = "reform-blaze";
-  version = "0.2.3";
+  version = "0.2.4.1";
   src = ./.;
-  buildDepends = [ base blaze-html blaze-markup reform text ];
+  libraryHaskellDepends = [
+    base blaze-html blaze-markup reform text
+  ];
   homepage = "http://www.happstack.com/";
   description = "Add support for using blaze-html with Reform";
   license = stdenv.lib.licenses.bsd3;
--- shell.nix
+++ shell.nix
@@ -1,16 +1,30 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, blaze-html, blaze-markup, reform, stdenv
-             , text
-             }:
-             mkDerivation {
-               pname = "reform-blaze";
-               version = "0.2.3";
-               src = ./.;
-               buildDepends = [ base blaze-html blaze-markup reform text ];
-               homepage = "http://www.happstack.com/";
-               description = "Add support for using blaze-html with Reform";
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, blaze-html, blaze-markup, reform
+      , stdenv, text
+      }:
+      mkDerivation {
+        pname = "reform-blaze";
+        version = "0.2.4.1";
+        src = ./.;
+        libraryHaskellDepends = [
+          base blaze-html blaze-markup reform text
+        ];
+        homepage = "http://www.happstack.com/";
+        description = "Add support for using blaze-html with Reform";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
 in
-  pkg.env
+
+  if pkgs.lib.inNixShell then drv.env else drv
